f80182d77bb14a6bb5c1a101d188612caedaae25
Cvetomir Cankov <cankov.chris@gmail.com>
2016-09-01 08:30:09 +0000

    book appointments

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/* Header */
+// Load modules
+const express = require("express");
+const app = express();
+const fs = require("fs");
+//const $ = require("jquery");
+
+// Port & IP Constants
+const ENVPORT = process.env.PORT;
+const ENVIP = process.env.IP;
+const dirname = "data";
+var users = JSON.parse(fs.readFileSync(dirname+'/'+'users.json','utf8'));
+
 /* Object Constructors */
 // User
 function User(id, name, sp) {
@@ -5,34 +18,43 @@ function User(id, name, sp) {
     this.Name = name;
     this.serviceProvider = sp;
 }
-//
+// Appointment
+function Appointment(id, consumerID, providerID, dateTime) {
+    this.id = id;
+    this.consumerID = consumerID;
+    this.providerID = providerID;
+    this.dateTime = dateTime;
+}
 
-/* Main */
-// Load modules
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const $ = require("jquery");
-
-// Port & IP Constants
-const ENVPORT = process.env.PORT;
-const ENVIP = process.env.IP;
-const dirname = "data";
-
-app.get('/listUsers', function(req, res) {
-    fs.readFile(dirname + "/" + "users.json", 'utf8',function(err, data) {
-        if (err) { return console.log(err) }
-        console.log(data);
-        res.end(data);
-    });
-});
-
+/* Web API methods */
+// Add a new user with unique id; takes parameters name & sp
 app.get('/addUser/:name/:sp', function(req, res) {
     fs.readFile( dirname + "/" + "users.json", 'utf8', function (err, data) {
       if (err) { res.end("Unable to read user data from disk.") }
       data = JSON.parse( data );
-      data[data.length] = new User(data.length+1,req.params.name, req.params.sp === "false" || req.params.sp === "0" ? false :  true);
+      var user = new User(data.length+1,req.params.name, req.params.sp === "false" || req.params.sp === "0" ? false :  true);
+      data[data.length] = user;
       fs.writeFile(dirname + "/"+"users.json",JSON.stringify(data),'utf8', function(err){
+          if (err) { return res.end("Unable to commit new user to database.");}
+      });
+      users = data;
+      console.log( data );
+      res.end( JSON.stringify(user));
+   });
+});
+
+// Book an appointment with specified user; takes parameters consumerID, providerID & dateTime
+app.get('/bookAppt/:consumerID/:providerID/:dateTime', function(req, res) {
+    fs.readFile( dirname + "/" + "appointments.json", 'utf8', function (err, data) {
+      if (err) { return res.end("Unable to read user data from disk."); }
+      var timestamp = Date.parse(req.params.dateTime);
+      if (!users[Number(req.params.providerID)-1].serviceProvider) {return res.end("Requested user is not a valid service provider.");}
+      if (isNaN(timestamp)) { return res.end("Invalid date parameter."); }
+      var dateTime = new Date(timestamp);
+      data = JSON.parse( data );
+      data[data.length] = new Appointment(data.length+1,req.params.consumerID,req.params.providerID,dateTime);
+      
+      fs.writeFile(dirname + "/"+"appointments.json",JSON.stringify(data),'utf8', function(err){
           if (err) {res.end("Unable to commit new user to database.");}
       });
       console.log( data );
@@ -40,24 +62,34 @@ app.get('/addUser/:name/:sp', function(req, res) {
    });
 });
 
+// Delete user with specified id; takes parameter id
 app.get('/deleteUser/:id', function(req, res) {
     fs.readFile( dirname + "/" + "users.json", 'utf8', function (err, data) {
       if (err) { res.end("Unable to read user data from disk.") }
       data = JSON.parse( data );
       data[req.params.id-1] = {};
       fs.writeFile(dirname + "/"+"users.json",JSON.stringify(data),'utf8', function(err){
-          if (err) {res.end("Unable to commit new user to database.");}
+          if (err) {return res.end("Unable to commit new user to database.");}
       });
+      users = data;
       console.log( data );
-      res.end( JSON.stringify(data));
+      res.end( JSON.stringify({}));
    });
 });
 
+// List all users
+app.get('/listUsers', function(req, res) {
+    fs.readFile(dirname + "/" + "users.json", 'utf8',function(err, data) {
+        if (err) { return res.end(err); }
+        console.log(data);
+        res.end(data);
+    });
+});
 
+// List details of user with specified id; takes parameter id
 app.get('/:id', function (req, res) {
-   // First read existing users.
    fs.readFile( dirname + "/" + "users.json", 'utf8', function (err, data) {
-      if (err) { return console.log(err) }
+      if (err) { return res.end(err); }
       var users = JSON.parse( data );
       var user = users[req.params.id-1];
       console.log( user );
@@ -65,6 +97,8 @@ app.get('/:id', function (req, res) {
    });
 });
 
+/* Server */
+// Initialize web server
 var server = app.listen(ENVPORT, function() {
    var host = server.address().address;
    var port = server.address().port;
